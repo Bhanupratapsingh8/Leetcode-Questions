5b2714dca803c4ed990531328745773b55371188
Bhanu Pratap Singh <115988757+Bhanupratapsingh8@users.noreply.github.com>
2024-10-05 02:06:59 +0530

    Time: 0 ms (100%), Space: 7.8 MB (91.5%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> result;
+        for (int num = left; num <= right; num++) {
+            if (isSelfDividing(num)) {
+                result.push_back(num);
+            }
+        }
+        return result;
+    }
+
+private:
+    bool isSelfDividing(int num) {
+        int original = num;
+        while (num > 0) {
+            int digit = num % 10;
+            if (digit == 0 || original % digit != 0) {
+                return false;
+            }
+            num /= 10;
+        }
+        return true;
+    }
+};
