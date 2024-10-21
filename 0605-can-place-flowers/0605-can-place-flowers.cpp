e15ff30c9f67f6a1aff15826295513c40f17ca65
Bhanu Pratap Singh <115988757+Bhanupratapsingh8@users.noreply.github.com>
2024-10-22 00:40:11 +0530

    Time: 0 ms (100%), Space: 23.2 MB (7%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -3,20 +3,20 @@ public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         int length = flowerbed.size();
         for (int i = 0; i < length; ++i) {
-            // Check if the current plot is empty and adjacent plots are also empty or non-existent
+            
             if (flowerbed[i] == 0 &&
                 (i == 0 || flowerbed[i - 1] == 0) &&
                 (i == length - 1 || flowerbed[i + 1] == 0)) {
-                // Plant a flower
+                
                 flowerbed[i] = 1;
                 --n;
-                // Early exit if all flowers are planted
+                
                 if (n == 0) {
                     return true;
                 }
             }
         }
-        // If not all flowers could be planted, return false
+        
         return n <= 0;
     }
 };
