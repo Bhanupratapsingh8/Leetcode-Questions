43f6bf7cf4195aaa6fb87642facd48454b3503ab
Bhanu Pratap Singh <115988757+Bhanupratapsingh8@users.noreply.github.com>
2024-10-28 22:27:06 +0530

    Time: 4 ms (32.96%), Space: 13.3 MB (28.89%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -3,8 +3,6 @@ public:
     int thirdMax(vector<int>& nums) {
         set<int> s(nums.begin(), nums.end());
         if (s.size() < 3) return *s.rbegin();
-        auto it = s.rbegin();
-        advance(it, 2);
-        return *it;
+        return *next(s.rbegin(), 2);
     }
 };
