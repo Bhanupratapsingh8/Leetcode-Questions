e60de10e0ec01cc4cbd7b924f063680bd6ef8338
Bhanu Pratap Singh <115988757+Bhanupratapsingh8@users.noreply.github.com>
2024-11-02 16:07:25 +0530

    Time: 14 ms (18.09%), Space: 13 MB (62.66%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,61 +1,14 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int count=0;
-        for(int i=0; i<s.length(); i++)
-        {
-            if(s[i]=='I'){
-                if(s[i+1]=='V'){
-                    count+=4;
-                    i++;
-                }
-                else if(s[i+1]=='X'){
-                    count+=9;
-                    i++;
-                }
-                else{
-                    count+=1;
-                }
-            }
-            else if(s[i]=='X'){
-                if(s[i+1]=='L'){
-                    count+=40;
-                    i++;
-                }
-                else if(s[i+1]=='C'){
-                    count+=90;
-                    i++;
-                }
-                else{
-                    count+=10;
-                }
-            }
-            else if (s[i]=='C'){
-                if(s[i+1]=='D'){
-                    count+=400;
-                    i++;
-                }
-                else if(s[i+1]=='M'){
-                    count+=900;
-                    i++;
-                }
-                else{
-                    count+=100;
-                }
-            }
-            else if(s[i]=='V'){
-                count+=5;
-            }
-            else if(s[i]=='L'){
-                count+=50;
-            }
-            else if(s[i]=='D'){
-                count+=500;
-            }
-            else if(s[i]=='M'){
-                count+=1000;
-            }
+        unordered_map<char, int> roman = {{'I', 1}, {'V', 5}, {'X', 10},{'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+        int total = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && roman[s[i]] < roman[s[i + 1]])
+                total -= roman[s[i]];
+            else
+                total += roman[s[i]];
         }
-        return count;
-    }
+        return total;
+    }
 };
